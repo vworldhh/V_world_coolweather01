5fdb7dd88b728daf9640655dbc05cc539ac896cd
e <2401922716@qq.com>
2017-06-01 15:31:16 +0800

    SQL and Bean

---
+++ app/src/main/java/www/v_world/com/v_world_weather02/db/Province.java
@@ -0,0 +1,36 @@
+package www.v_world.com.v_world_weather02.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/6/1.
+ */
+public class Province extends DataSupport{
+    private int id;
+    private String ProvinceName;
+    private int ProvinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return ProvinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        ProvinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return ProvinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        ProvinceCode = provinceCode;
+    }
+}
